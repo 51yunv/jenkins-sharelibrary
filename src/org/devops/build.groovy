fa4b03855ae48fa0fbcff6b316d6fda9685685ff
devops <admin@example.com>
2020-05-15 16:08:08 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -10,10 +10,7 @@ def Build(buildType,buildShell){
     buildHome= tool buildTools[buildType]
     
     if ("${buildType}" == "npm"){
-        sh  """ 
-            export NODE_HOME=${buildHome} 
-            export PATH=\$NODE_HOME/bin:\$PATH 
-            ${buildHome}/bin/${buildType} ${buildShell}"""
+        sh "${buildHome}/bin/${buildType} ${buildShell}"
     } else {
         sh "${buildHome}/bin/${buildType}  ${buildShell}"
     }
