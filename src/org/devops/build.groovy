8df59f1f08deefaa6468f4634e8a02c5a7401341
devops <admin@example.com>
2020-05-15 16:01:45 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,6 +1,20 @@
 package org.devops
 
-def Build(buildType){
-  println("当前选择的构建类型为${buildType}")
-  sh "$buildType -v"
+
+//构建类型
+def Build(buildType,buildShell){
+    def buildTools = ["mvn":"m2","ant":"ant","gradle":"gradle","npm":"npm"]
+    
+    
+    println("当前选择的构建类型为 ${buildType}")
+    buildHome= tool buildTools[buildType]
+    
+    if ("${buildType}" == "npm"){
+        sh  """ 
+            export NODE_HOME=${buildHome} 
+            export PATH=\$NODE_HOME/bin:\$PATH 
+            ${buildHome}/bin/${buildType} ${buildShell}"""
+    } else {
+        sh "${buildHome}/bin/${buildType}  ${buildShell}"
+    }
 }
