f3cdb500c6ef75ce24ae064498cca97cedf5f0e5
devops <admin@example.com>
2020-05-17 23:39:38 +0800

    Update gitlab.groovy

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -4,13 +4,13 @@ package org.devops
 def HttpReq(reqType,reqUrl,reqBody){
     gitlabServer = "http://gitlabtest.goschainccap.com/api/v4"
     withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlabToken')]) {
-        result = httpRequest httpMode: "${reqType}",
+        httpRequest httpMode: "${reqType}",
                              customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]], 
                              //contentType: 'APPLICATION_JSON',
                              url: "${gitlabServer}/${reqUrl}",
                              wrapAsMultipart: false
     }
-    return result
+    //return result
     
 }
 
@@ -19,5 +19,5 @@ def ChangeCommitStatus(projectId,commitSha,status){
     commitApi = "projects/${projectId}/statuses/${commitSha}?state=${status}"
     response = HttpReq('POST',commitApi,'')
     println("返回的结果为：" + response)
-    return response
+    //return response
 }
