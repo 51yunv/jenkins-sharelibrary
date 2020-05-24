9b2d0b43d56266655850685e20732abc1768d625
devops <1135960569@qq.com>
2020-05-24 17:09:07 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -25,7 +25,7 @@ def sonarScan(projectName,projectDesc,projectPath){
         """
     }
     def qg = waitForQualityGate()
-    if (qg.status != 'OK') {
-      error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    }
+    //if (qg.status != 'OK') {
+    //  error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    //}
 }
