f55ef59b80f8e2a4add2a324c09ede83c6f2d135
devops <1135960569@qq.com>
2020-05-24 17:08:27 +0800

    Add new file

---
+++ src/org/devops/sonarapi.groovy
@@ -0,0 +1,23 @@
+packge org.devops
+
+//封装http请求
+def HttpReq(reqType,reqUrl,reqBody){
+    sonarServer = "http://sonarqubetest.goschainccap.com/api"
+    result = httpRequest authentication: 'sonar-admin', 
+             httpMode: reqType
+             contentType: 'APPLICATION_JSON', 
+             responseHandle: 'NONE', 
+             url: "${sonarServer}/${reqUrl}", 
+             wrapAsMultipart: false
+    return result
+}
+
+//获取sonar质量阈状态
+
+def GetrojectStatus(projectName){
+    apiUrl="project_branches/list?project=${projectName}"
+    response = HttpReq("GET",apiUrl,'')
+    response = readJSON text: """${response.content}"""
+    println(response)
+    return response
+}
