bfbf7e1ffe0e1b5f0559326cd1b3a074e78284fc
devops <1135960569@qq.com>
2020-05-25 10:57:03 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -7,46 +7,20 @@ def sonarScan(projectName,projectDesc,projectPath,runOpts='noset',projectId='',c
     sonarDate = sh returnStdout: true, script: 'date +%F_%T'
     sonarDate = sonarDate - "\n"
     
-    
-    
     withSonarQubeEnv(credentialsId: 'sonar-token') {
-        if(runOpts != "GitlabPush"){
-            sh """
-                ${sonar_home}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.projectVersion=${sonarDate} \
-                -Dsonar.ws.timeout=30 \
-                -Dsonar.projectDescription=${projectDesc} \
-                -Dsonar.links.homepage=http://www.baidu.com \
-                -Dsonar.sources=${projectPath} \
-                -Dsonar.sourceEncoding=UTF-8 \
-                -Dsonar.java.binaries=target/classes \
-                -Dsonar.java.test.binaries=target/test-classes \
-                -Dsonar.java.surefire.report=target/surefire-reports \
-                -Dsonar.analysis.mode=preview \
-                -Dsonar.gitlab.project_id=${projectId} \
-                -Dsonar.gitlab.commit_sha=${commitSha} \
-                -Dsonar.gitlab.ref_name=${branchName}
-            """
-        }else{
-            sh """
-                ${sonar_home}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.projectVersion=${sonarDate} \
-                -Dsonar.ws.timeout=30 \
-                -Dsonar.projectDescription=${projectDesc} \
-                -Dsonar.links.homepage=http://www.baidu.com \
-                -Dsonar.sources=${projectPath} \
-                -Dsonar.sourceEncoding=UTF-8 \
-                -Dsonar.java.binaries=target/classes \
-                -Dsonar.java.test.binaries=target/test-classes \
-                -Dsonar.java.surefire.report=target/surefire-reports 
-            """
-        }
+        sh """
+            ${sonar_home}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectVersion=${sonarDate} \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDesc} \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=${projectPath} \
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes \
+            -Dsonar.java.surefire.report=target/surefire-reports 
+        """
         
     }
-    //def qg = waitForQualityGate()
-    //if (qg.status != 'OK') {
-    //  error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    //}
 }
