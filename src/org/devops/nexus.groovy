118baf6da970d83a2a192f6f846a6de5133ab521
devops <1135960569@qq.com>
2020-05-26 23:17:24 +0800

    Update nexus.groovy

--- src/org/devops/nexus.groovy
+++ src/org/devops/nexus.groovy
@@ -71,7 +71,7 @@ def ArtifactUpdate(updateType,artifactUrl){
         env.jarName = artifactUrl[-1]
         env.pomVersion = artifactUrl[-2]
         env.pomArtifact = artifactUrl[-3]
-        env.pomPackaging = artifactUrl[-1]
+        env.pomPackaging = artifactUrl[-1].split(".").toList()[-1]
         env.pomGroupId = artifactUrl[-4..0].join(".")
         println("${pomGroupId}##${pomArtifact}##${pomVersion}##${pomPackaging}")
         //上传制品
