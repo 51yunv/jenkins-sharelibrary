042b4e739068d4378d98df761b2c61b72cd44cf5
devops <1135960569@qq.com>
2020-05-27 15:42:38 +0800

    Add new file

---
+++ src/org/devops/nexusapi.groovy
@@ -0,0 +1,24 @@
+package org.devops
+
+
+//封装http请求
+def HttpRequest(reqType,reqUrl,reqBody){
+    nexusServer = "http://nexustest.goschainccap.com/service/rest"
+    result = httpRequest authentication: 'nexus-admin',
+                consoleLogResponseBody: true, 
+                contentType: 'APPLICATION_JSON', 
+                httpMode: "${reqType}",
+                requestBody: "${reqBody}", 
+                responseHandle: 'NONE', 
+                url: "${nexusServer}/${reqUrl}", 
+                wrapAsMultipart: false
+    return result
+}
+
+//获取仓库中所有组件
+def GetRepoComponents(repoName){
+    apiUrl = "/v1/comments?repository=${repoName}"
+    response = HttpRequest("GET",apiUrl,"")
+    response = readJSON text: "${response.content}"
+    println(response["items"].size())   
+}
