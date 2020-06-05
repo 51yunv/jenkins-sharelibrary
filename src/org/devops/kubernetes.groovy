391ee1229c69fb21d8582727a2f8c859d4dc98e4
devops <1135960569@qq.com>
2020-06-05 16:30:20 +0800

    Update kubernetes.groovy

--- src/org/devops/kubernetes.groovy
+++ src/org/devops/kubernetes.groovy
@@ -4,7 +4,7 @@ package org.devops
 def HttpReq(reqType,reqUrl,reqBody){
     apiUrl = "https://172.17.138.183:6443/apis/apps/v1"
     withCredentials([string(credentialsId: 'k8s-admin-token', variable: 'k8stoken')]) {
-        result = httpRequest consoleLogResponseBody: false, 
+        result = httpRequest consoleLogResponseBody: true, 
                              contentType: 'TEXT_HTML', 
                              customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer $k8stoken"],[maskValue: false, name: 'Content-Type', value: 'application/yaml'],[maskValue: false, name: 'Accept', value: 'application/yaml']], 
                              httpMode: "${reqType}", 
@@ -16,22 +16,6 @@ def HttpReq(reqType,reqUrl,reqBody){
     }
     return result
 }
-
-/*def HttpReq(reqType,reqUrl,reqBody){
-    def apiServer = "https://172.17.138.183:6443/apis/apps/v1"
-    withCredentials([string(credentialsId: 'k8s-admin-token', variable: 'k8stoken')]) {
-      result = httpRequest customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${k8stoken}"],
-                                           [maskValue: false, name: 'Content-Type', value: 'application/yaml'], 
-                                           [maskValue: false, name: 'Accept', value: 'application/yaml']], 
-                httpMode: reqType, 
-                consoleLogResponseBody: true,
-                ignoreSslErrors: true, 
-                requestBody: reqBody,
-                url: "${apiServer}/${reqUrl}"
-                //quiet: true
-    }
-    return result
-}*/
 
 //获取deployment
 def GetDeployment(namespace,deployment){
