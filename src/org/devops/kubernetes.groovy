5b9c83172046389f3b8cd0621870cc00c013e256
devops <1135960569@qq.com>
2020-06-05 16:18:50 +0800

    Update kubernetes.groovy

--- src/org/devops/kubernetes.groovy
+++ src/org/devops/kubernetes.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 //封装对k8s的http请求
-def HttpReq(reqType,reqUrl,reqBody){
+/*def HttpReq(reqType,reqUrl,reqBody){
     apiUrl = "https://172.17.138.183:6443/apis/apps/v1"
     withCredentials([string(credentialsId: 'k8s-admin-token', variable: 'k8stoken')]) {
         result = httpRequest consoleLogResponseBody: true, 
@@ -12,6 +12,22 @@ def HttpReq(reqType,reqUrl,reqBody){
                              responseHandle: 'NONE', 
                              url: "${apiUrl}/${reqUrl}", 
                              wrapAsMultipart: false
+    }
+    return result
+}*/
+
+def HttpReq(reqType,reqUrl,reqBody){
+    def apiServer = "https://172.17.138.183:6443/apis/apps/v1"
+    withCredentials([string(credentialsId: 'k8s-admin-token', variable: 'k8stoken')]) {
+      result = httpRequest customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${k8stoken}"],
+                                           [maskValue: false, name: 'Content-Type', value: 'application/yaml'], 
+                                           [maskValue: false, name: 'Accept', value: 'application/yaml']], 
+                httpMode: reqType, 
+                consoleLogResponseBody: true,
+                ignoreSslErrors: true, 
+                requestBody: reqBody,
+                url: "${apiServer}/${reqUrl}"
+                //quiet: true
     }
     return result
 }
