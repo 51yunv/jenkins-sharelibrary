7ba9e8b62529d727f506be65170c0d22e7d4101a
51yunv <1135960569@qq.com>
2020-06-16 13:48:56 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,17 +1,8 @@
 package org.devops
 
-
-//构建类型
 def Build(buildType,buildShell){
-    def buildTools = ["mvn":"m2","ant":"ant","gradle":"gradle","npm":"npm"]
-    
-    
-    println("当前选择的构建类型为 ${buildType}")
-    buildHome= tool buildTools[buildType]
-    
-    if ("${buildType}" == "npm"){
-        sh "${buildHome}/bin/${buildType} ${buildShell}"
-    } else {
-        sh "${buildHome}/bin/${buildType}  ${buildShell}"
-    }
+    buildTools = ["mvn":"m2","ant":"ant","gradle":"gradle","node":"nodejs","npm":"nodejs"]
+    //println("当前选择的构建类型为${buildType}")
+    buildHome = tool buildTools[buildType]
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
